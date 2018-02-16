5b4c98f3ed708515f0b1c87413ab3298342c270e
Uchenna Ohaeto <uoohaeto@gmail.com>
2018-02-15 19:49:55 -0500

    Made some style changes

--- login.php
+++ login.php
@@ -8,19 +8,19 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="styles/new_style.css" media="all" </link>
-  <h1 align="center"> Monsta Codes </h1>
+  <h1 align="center"> <a href="index.php">Monsta Codes</a> </h1> <!-- TODO Possibly replace with site logo -->
 </head>
 
 <body>
   <div id="login" class="login_div">
     <form action="customer_register.php" method="post" enctype="multipart/form-data">
-      <label for="login_email"><b>Username</b></label>
+      <label for="login_email"><b><font face="helvetica">Username</font></b></label>
       <input type="text" name="login_email" required placeholder="Enter email"/>
-      <label for="login_pass"><b>Password</b></label>
+      <label for="login_pass"><b><font face="helvetica">Password</font></b></label>
       <input type="password" name="login_pass" required placeholder="password"/>
       <button class="login_button"> Log In </button>
-      <p> New user? <a href="/register.php"> Sign Up </a> </p>
     </form>
+    <p><font face="helvetica">New User? </font><a href="/register.php"><b><font face="helvetica">Sign Up</font></b> </a> </p>
   </div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -8,31 +8,31 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="styles/new_style.css" media="all" </link>
-  <h1 align="center"> Monsta Codes </h1>
+  <h1 align="center"> <a href="index.php">Monsta Codes</a> </h1> <!-- TODO Possibly replace with site logo -->
 </head>
 
 <body>
   <div id="register" class="login_div">
     <form>
-      <label for="first_name"><b>First Name</b></label>
+      <label for="first_name"><b><font face="helvetica">First Name</font></b></label>
       <input type="text" name="first_name" required placeholder="First Name"/>
 
-      <label for="last_name"><b>Last name</b></label>
+      <label for="last_name"><b><font face="helvetica">Last Name</font></b></label>
       <input type="text" name="last_name" required placeholder="Last Name"/>
 
-      <label for="email"><b>Email</b></label>
+      <label for="email"><b><font face="helvetica">Email Address</font></b></label>
       <input type="text" name="email" required placeholder="someemail@emails.com"/>
 
-      <label for="con_email"><b>Re-enter Email</b></label>
+      <label for="con_email"><b><font face="helvetica">Re-enter Email Address</font></b></label>
       <input type="text" name="con_email" required placeholder="someemail@emails.com"/>
 
-      <label for="password"><b>Password</b></label>
+      <label for="password"><b><font face="helvetica">Password</font></b></label>
       <input type="text" name="password" required/>
 
-      <label for="re_password"><b>Re-enter Password</b></label>
+      <label for="re_password"><b><font face="helvetica">Re-enter Password</font></b></label>
       <input type="text" name="re_password" required/>
 
-      <button class="signup_button"> Submit </button>
+      <button class="login_button"> Submit </button>
     </form>
   </div>
 </body>
