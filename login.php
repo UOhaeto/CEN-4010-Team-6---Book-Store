c79a835b151a9c90e42f3f9687478e4cf114fddc
Uchenna Ohaeto <uoohaeto@gmail.com>
2018-02-16 22:43:17 -0500

    Fixed Search bar

--- login.php
+++ login.php
@@ -15,9 +15,9 @@
   <div id="login" class="login_div">
     <form action="customer_register.php" method="post" enctype="multipart/form-data">
       <label for="login_email"><b><font face="helvetica">Username</font></b></label>
-      <input type="text" name="login_email" required placeholder="Enter email"/>
+      <input class="user" type="text" name="login_email" required placeholder="Enter email"/>
       <label for="login_pass"><b><font face="helvetica">Password</font></b></label>
-      <input type="password" name="login_pass" required placeholder="password"/>
+      <input class="user" type="password" name="login_pass" required placeholder="password"/>
       <button class="login_button"> Log In </button>
     </form>
     <p><font face="helvetica">New User? </font><a href="/register.php"><b><font face="helvetica">Sign Up</font></b> </a> </p>
--- register.php
+++ register.php
@@ -13,24 +13,24 @@
 
 <body>
   <div id="register" class="login_div">
-    <form>
+    <form> 
       <label for="first_name"><b><font face="helvetica">First Name</font></b></label>
-      <input type="text" name="first_name" required placeholder="First Name"/>
+      <input class="user" type="text" name="first_name" required placeholder="First Name"/>
 
       <label for="last_name"><b><font face="helvetica">Last Name</font></b></label>
-      <input type="text" name="last_name" required placeholder="Last Name"/>
+      <input class="user" type="text" name="last_name" required placeholder="Last Name"/>
 
       <label for="email"><b><font face="helvetica">Email Address</font></b></label>
-      <input type="text" name="email" required placeholder="someemail@emails.com"/>
+      <input class="user" type="text" name="email" required placeholder="someemail@emails.com"/>
 
       <label for="con_email"><b><font face="helvetica">Re-enter Email Address</font></b></label>
-      <input type="text" name="con_email" required placeholder="someemail@emails.com"/>
+      <input class="user" type="text" name="con_email" required placeholder="someemail@emails.com"/>
 
       <label for="password"><b><font face="helvetica">Password</font></b></label>
-      <input type="text" name="password" required/>
+      <input class="user" type="text" name="password" required/>
 
       <label for="re_password"><b><font face="helvetica">Re-enter Password</font></b></label>
-      <input type="text" name="re_password" required/>
+      <input class="user" type="text" name="re_password" required/>
 
       <button class="login_button"> Submit </button>
     </form>
