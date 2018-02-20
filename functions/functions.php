ba1a39c0a0e286488a832f98ba3c9f7356e77935
Ronny Pacheco <ronny.pach@gmail.com>
2018-02-20 12:21:08 -0500

    Worked on Register page
    --Change type from text to password, on password fields.
    --Added conection to database.
    --Setup code to fetch user input into variables, to later send into the database.
    
    --Missing validation between re-entering emails and passwords.
    
    --got rid of white space on insert_book page.

--- functions/functions.php
+++ functions/functions.php
@@ -7,11 +7,23 @@
 		echo "Failed to connect to MySQL: " . mysqli_connect_errno();
 	}
 
+//getting customer IP
+	function getIp() {
+	$ip = $_SERVER['REMOTE_ADDR'];
+
+	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	}
+
+	return $ip;
+}
+
 	//function to get genres
 	function getGenres(){
 
 		if(!isset($_GET['genre'])){
-
 
 			//make variable global
 			global $con;
@@ -19,18 +31,13 @@
 			//variable that gets items from book_genres table
 			$get_genres = "select * from book_genres order by genre_type";
 
-
 			$run_genres =  mysqli_query($con, $get_genres);
 
 			//loop that runs depending on the amount of rows on the book_genres table
-
 			while($row_genres=mysqli_fetch_array($run_genres)){
-
-
 				//fetching the information from the genre_type table on database
 				$genre_id = $row_genres['genre_id'];
 				$genre_type = $row_genres['genre_type'];
-
 				//display genres
 				echo "<li><a href='genres.php?genre=$genre_id'>$genre_type </a></li>";
 			}
@@ -42,23 +49,15 @@
 	//function to display books on main page.
 	function getBook(){
 
-
-
 		if(!isset($_GET['genre'])){
-
-
 
 		//connection to db
 			global $con;
-
 			//get 6 random books
 			$get_b = "select * from books order by RAND() LIMIT 0,6";
 
-
 			$run_b = mysqli_query($con, $get_b);
-
 			while($row_b=mysqli_fetch_array($run_b)){
-
 					//initializing variable with book name.
 					$b_title = $row_b['book_title'];
 					$b_author = $row_b['author'];
@@ -71,9 +70,6 @@
 					//primary key
 					//used to display individual details page.
 					$b_isbn = $row_b['isbn'];
-
-
-
 
 					echo "
 						<div id='single_book'>
@@ -103,9 +99,6 @@
 
 		if(isset($_GET['genre'])){
 
-
-
-
 			//getting book genre from url
 			$book_genre = $_GET['genre'];
 
@@ -114,19 +107,13 @@
 
 			//get 6 random books
 			$get_books_genre = "select * from books where genre ='$book_genre' LIMIT 0,6";
-
-
 			$run_books_genre = mysqli_query($con, $get_books_genre);
-
 			$book_counter = mysqli_num_rows($run_books_genre);
 
 			if($book_counter == 0){
 					echo "<h2>There are no books on this genre!</h2>";
-
 			}
-
 			else{
-
 				while($row_books_genre=mysqli_fetch_array($run_books_genre)){
 
 						//initializing variable with book name.
@@ -137,16 +124,9 @@
 					$b_price = $row_books_genre['price'];
 					$b_rating = $row_books_genre['rating'];
 					$b_image = $row_books_genre['book_image'];
-
-							//primary key
+						//primary key
 						//used to display individual datails page.
 					$b_isbn = $row_books_genre['isbn'];
-
-
-
-
-
-
 					echo "
 						<div id='single_book'>
 
@@ -164,15 +144,8 @@
 
 						";
 				}
-
 			}
-
 		}
 	}
-
-
-
-
-
 
 ?>
--- register.php
+++ register.php
@@ -3,6 +3,7 @@
 
 <?php
 	include("functions/functions.php");
+	include("includes/db.php")
 ?>
 
 <head>
@@ -13,7 +14,8 @@
 
 <body>
   <div id="register" class="login_div">
-    <form>
+    <form action="register.php" method="post" enctype="multipart/form-data">
+
       <label for="first_name"><b>First Name</b></label>
       <input type="text" name="first_name" required placeholder="First Name"/>
 
@@ -27,13 +29,63 @@
       <input type="text" name="con_email" required placeholder="someemail@emails.com"/>
 
       <label for="password"><b>Password</b></label>
-      <input type="text" name="password" required/>
+      <input type="password" name="password" required/>
 
       <label for="re_password"><b>Re-enter Password</b></label>
-      <input type="text" name="re_password" required/>
+      <input type="password" name="re_password" required/>
 
-      <button class="signup_button"> Submit </button>
+      <!--
+			<button class="signup_button"> Submit </button>
+			-->
+			<input type="submit" name="register_user" value="Create Account" />
     </form>
   </div>
 </body>
 </html>
+
+<?php
+
+	if(isset($_POST['register_user'])){
+
+	$ip = getIp();
+
+	//fetching user variables.
+	$u_first_name = addslashes($_POST['first_name']);
+	$u_last_name = addslashes($_POST['last_name']);
+
+	$u_email = addslashes($_POST['email']);
+	$u_con_email = addslashes($_POST['con_email']);
+
+	$u_password = addslashes($_POST['password']);
+	$u_re_password_ = addslashes($_POST['re_password']);
+
+	$user_id = 3;
+	$user_username = "user3";
+	$user_address = "123 street";
+	$user_city = "Miami";
+	$user_state = "Florida";
+	$user_zip = "33188";
+	$user_nickname = "br";
+
+/*
+	$insert_u = "insert into users (fName, lName, email, password)
+	values ('$u_first_name','$u_last_name','$u_con_email','$u_re_password_') ";
+*/
+
+$insert_u = "insert into users (userID, username, password, fName, lName, email, homeStreet, homeCity, homeZip, nickname)
+values ('$user_id','$user_username','$u_password','$u_first_name','$u_last_name','$u_email','$user_address','$user_city', '$user_zip', '$user_nickname') ";
+
+	$run_c = mysqli_query($con, $insert_u);
+
+	if($run_c){
+		echo "<script>alert('user registered successfully!')</script>";
+	}
+	else{
+		echo "<script>alert('could not register user :\')</script>";
+	}
+
+}
+
+
+
+?>
