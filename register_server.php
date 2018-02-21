d1c25cdaeb629886c6ea57a2fc837f8076b94419
Ronny Pacheco <ronny.pach@gmail.com>
2018-02-21 17:30:54 -0500

    Added Randomizer for Users
    --Fixed problem that prevented info from being added into the database.

--- register_server.php
+++ register_server.php
@@ -9,7 +9,7 @@
 		//maybe use later.
 		//$ip = getIp();
 		//add randomizer for id
-	$user_id = "4";
+	$user_id = rand(1,10000);
 	$user_username = addslashes($_POST['username']);
 	$user_address = addslashes($_POST['address']);
 	$user_city = addslashes($_POST['city']);
@@ -54,15 +54,13 @@ if(preg_match("/^.*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$/", $u_password)
 
 if(preg_match("#[0-9]{5}#", $user_zip) === 0){
 array_push($errors, "Zip must be 5 numbers.");
-echo $user_zip;
+
 //$errZip = '<p class="errText">Zip must be 4 digits</p>';
 }
 /*
 	$insert_u = "insert into users (fName, lName, email, password)
 	values ('$u_first_name','$u_last_name','$u_con_email','$u_re_password_') ";
 */
-
-
 
 // a user does not already exist with the same username and/or email
 $user_check_query = "SELECT * FROM users WHERE username='$user_username' OR email='$u_email' LIMIT 1";
@@ -80,15 +78,15 @@ if ($user) { // if user exists
 }
 
   if (count($errors) == 0) {
+
+		/*
 			$insert_u = "insert into users (userID, username, password, fName, lName, email, homeStreet, homeCity,homeState, homeZip, nickname)
 			values ('$user_id','$user_username','$u_password','$u_first_name','$u_last_name','$u_email','$user_address','$user_city', '$user_state' '$user_zip', '$user_nickname') ";
+*/
+			echo $insert_u = "insert into users (userID, username, password, fName, lName, email, homeStreet, homeCity, homeState, homeZip, nickname) values ('$user_id','$user_username','$u_password','$u_first_name','$u_last_name','$u_email','$user_address','$user_city', '$user_state', '$user_zip', '$user_nickname')";
 
 			$run_c = mysqli_query($con, $insert_u);
 
-
-			//echo $insert_book = "insert into books (book_title, author, author_bio, genre, release_date, price, year, publisher, description, quantity, rating, book_image) values ('$book_title','$author','$author_bio','$genre','$release_date','$price','$year','$publisher', '$description','$quantity', '$rating', '$book_image')";
-
-			echo $insert_u = "insert into users (userID, username, password, fName, lName, email, homeStreet, homeCity, homeState, homeZip, nickname) values ('$user_id','$user_username','$u_password','$u_first_name','$u_last_name','$u_email','$user_address','$user_city', '$user_state', '$user_zip', '$user_nickname')";
 
 			if($run_c){
 				echo "<script>alert('user registered successfully!')</script>";
@@ -99,7 +97,5 @@ if ($user) { // if user exists
 
 		}
 }
-
-
 
 ?>
