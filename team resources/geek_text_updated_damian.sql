05c093613c84d9e022fa3183e901ef4b8e40e7ea
dnieb002 <35749177+dnieb002@users.noreply.github.com>
2018-02-25 10:41:37 -0500

    Wrong SQL File
    -Corrected SQL file with deleted phone numbers table, and additional user table addition.

--- team resources/geek_text_updated_damian.sql
+++ team resources/geek_text_updated_damian.sql
@@ -63,14 +63,6 @@ CREATE TABLE `users` (
   UNIQUE KEY `userID_UNIQUE` (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `phonenumbers` (
-  `users_userID` int(10) unsigned NOT NULL,
-  `areaCode` varchar(45) NOT NULL,
-  `restOfNumber` varchar(45) NOT NULL,
-  KEY `fk_phonenumbers_users_idx` (`users_userID`),
-  CONSTRAINT `fk_phonenumbers_users` FOREIGN KEY (`users_userID`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `shippingaddresses` (
   `shippingaddressID` int(11) NOT NULL,
   `shippingStreet` varchar(45) NOT NULL,
@@ -102,15 +94,6 @@ INSERT INTO `books` (`book_title`, `isbn`, `author`, `author_bio`, `genre`, `rel
 ('The Da Vinci Code', 9, 'Dan Brown', '', '3', '2003-03-18', 11, 2003, 'Transworld', '', 150, 3, '220px-DaVinciCode.jpg'),
 ('Fifty Shades of Grey', 10, 'E. L. James', '', '4', '2011-05-01', 16, 2011, 'Random House', '', 200, 4234, 'Fifty shades of grey.jpg'),
 ('Harry Potter: Cursed Child', 11, 'J.K. Rowling', '', '1', '2018-02-21', 10, 2019, 'Crown/Archetype', '', 50, 5, 'Philosopher Stone.jpg');
-
-
-INSERT INTO `users` (`userID`, `username`, `password`, `fName`, `lName`, `email`, `homeStreet`, `homeCity`, `homeState`, `homeZip`, `nickname`) VALUES
-(0, 'user1', 'password1', 'Harry', 'Potter', 'harry@hogwarts.edu', '123 Abc St.', 'Magic City', 'Florida', '33133', 'hp'),
-(1, 'user2', 'password2', 'Hermione', 'Granger', 'hermione@hogwarts.edu', '456 Abc St.', 'Magic City', 'Florida', '33133', 'hg');
-
-INSERT INTO `phoneNumbers` (`users_userID`, `areaCode`, `restOfNumber`) VALUES
-(0, '305', '444-1212'),
-(1, '305', '555-2323');
 
 INSERT INTO `users` (`userID`, `username`, `password`, `fName`, `lName`, `email`, `homeStreet`, `homeCity`, `homeState`, `homeZip`, `nickname`, `entireNumber`) VALUES
 (0, 'user1', 'password1', 'Harry', 'Potter', 'harry@hogwarts.edu', '123 Abc St.', 'Magic City', 'Florida', '33133', 'hp', '305-444-1212'),
