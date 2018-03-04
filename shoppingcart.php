eee22479e5512d68361b329cc1856b3213f80db4
Sergio <SergCodes@users.noreply.github.com>
2018-03-04 17:05:58 -0500

    shopping cart updated
    The shopping cart has been fixed

--- shoppingcart.php
+++ shoppingcart.php
@@ -14,7 +14,6 @@
 <body>
 
 	<div class="content" >
-
 		<?php  cart(); ?>
 
 	<form action = "" method = "post" enctype="multipart/form-data">
@@ -64,7 +63,7 @@
 					?>
 
 					<tr align = "center">
-							<td><input type = "checkbox" name = "remove[]" value="<?php echo $pro_id; ?>"/></td>
+							<td><input type = "checkbox" name = "remove[]" value = "<?php echo $pro_id;?>"/></td>
 							<td><?php echo $book_tit; ?><br>
 							<img src='admin/book_images/<?php echo $book_img;?>' width ='100' height='120'/>
 							</td>
@@ -72,6 +71,9 @@
 							<?php
 
 							if(isset($_POST['update_cart'])){
+
+								//if quantity is set
+									if(is_numeric($_POST['quantity'])){
 
 											$qty = $_POST['quantity'];
 
@@ -82,8 +84,26 @@
 											$_SESSION['quantity'] = $qty;
 
 											$total = $total * $qty;
+										}
+										//checking if remove checkbox was set
+										if(isset($_POST['remove'])){
+										foreach ($_POST['remove'] as $remove_id) {
 
-							}
+											$delete_book = "delete from cart where book_id= '$remove_id' AND ip_add = '$ip'";
+
+											$run_delete = mysqli_query($con, $delete_book);
+
+											if($run_delete){
+												echo "<script>window.open('shoppingcart.php','_self')</script>";
+
+											}
+
+										}//end of for each
+
+									}//end of remove if
+
+							}//end of outer if
+
 
 							?>
 
@@ -117,42 +137,6 @@
 
 	</form>
 
-
-<!--code to delete and update the cart for user-->
-	<?php
-	function updatecart(){
-
-				global $con;
-
-				$ip = getIp();
-
-				if(isset($_POST['update_cart'])){
-
-						foreach ($_POST['remove'] as $remove_id) {
-
-							$delete_book = "delete from cart where book_id= '$remove_id' AND ip_add = '$ip'";
-
-							$run_delete = mysqli_query($con, $delete_book);
-
-							if($run_delete){
-
-								echo "<script>window.open('shoppingcart.php','_self')</script>";
-
-
-							}
-						}
-
-
-				}
-				if(isset($_POST['continue'])){
-
-					echo "<script>window.open('index.php','_self')</script>";
-
-				}
-
-		echo @$up_cart = updatecart();
-	}
-	?>
 
 	</div>
 
