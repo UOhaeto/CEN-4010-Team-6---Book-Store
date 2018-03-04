c83400165496cc5e55f41099121917a2be549e67
Sergio Remedio <35618670+SergCodes@users.noreply.github.com>
2018-03-04 16:39:54 -0500

    Changed code of shopping cart, working properly
    latest version of shopping cart

---
+++ authorize.php
@@ -0,0 +1,8 @@
+<?php
+
+	//Check where you are logged in
+	if(!isset($_SESSION['SESS_USERID']) || (trim($_SESSION['SESS_USERID']) == '')) {
+		header("location: login.php");
+		exit();
+	}
+?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+	include("html/header.php");
+	require_once('authorize.php');
+?>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="styles/new_style.css" media="all" </link>
+</head>
+<body>
+	  		<center><b><font color="white"> Welcome back
+		<?php
+			echo($_SESSION['SESS_USERNAME']);
+		?>! </b></center>
+	<div class="content" >
+
+	<?php cart(); ?>
+		Your Cart: <?php total_books();?> Total Price: <?php total_price(); ?>
+
+		<div id="book_container">
+			<?php getBook(); ?>
+		</div>
+
+	</div>
+
+</body>
+</html>
