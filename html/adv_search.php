b450beeaa013d1e2c8c09c6796ad2d5f62488db3
Ronny Pacheco <ronny.pach@gmail.com>
2018-03-30 13:47:02 -0400

    Added Sortning
    Types of Sorting Added:
    -Author
    -Price: high to low
    -Price: low to high
    - Year: old to new
    -Year: New to Old
    - Book Title.

---
+++ html/adv_search.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+	include("functions/functions.php");
+	session_start();
+?>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="styles/new_style.css" media="all">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+  <body>
+    <div class="topnav">
+      <div class="mini_topnav">
+        <a>Contact Us</a>
+		<?php
+			if(($_SESSION['SESS_USERID'] == 0)) {
+				echo '<a class="menu_link" href="login.php"> Login </a>';
+			}
+			else {
+        echo '<a href="logout.php">Logout</a>';
+				echo '<a href="myAccount.php">My Account</a>';
+			}
+		?>
+		</b>
+      </div>
+      <div class=topnav_mid>
+        <a href="index.php"><img src="images/Book Monster.png" alt="home_logo" width="250" height="80"></a>
+        <div class="search-container">
+          <form action="result.php" class="search_form">
+
+            <input type="text" name="search" placeholder="Search.." value=""/>
+            <button type="submit"><i class="fa fa-search"></i></button>
+							<div>
+							<label for="sort_label" style="display: block; width: 100px;"><b><font face="helvetica">Sort by</font></b></label>
+							<select name="sort">
+								<option value="">--</option>
+								<option value="author">Author</option>
+								<!--
+								<option value="rating">Book Rating</option>
+							-->
+
+								<option value="title">Book Title</option>
+								<option value="low-price">Price: Low to High</option>
+								<option value="high-price">Price: High to Low </option>
+								<option value="old-release">Release Year: Oldest to Newest</option>
+								<option value="new-release">Release Year: Oldest to Newest</option>
+
+							</select>
+						</div>
+          </form>
+        </div>
+      </div>
+      <div class="topnav_menu">
+        <a href="index.php">Home</a>
+        <a href="#news">Books</a>
+        <a href="#top_sellers">Top Sellers</a>
+		<a href="index2.php"> Rate a Book </a>
+        <a href="shoppingCart.php" style="float: right"> Shopping Cart</a>
+      </div>
+    </div>
+  </body>
+</html>
--- result.php
+++ result.php
@@ -4,7 +4,7 @@
 <?php
 
 	include("functions/functions.php");
-  echo file_get_contents("html/header.php");
+  echo file_get_contents("html/adv_search.php");
 ?>
 
 <head>
@@ -20,37 +20,79 @@
 				if(isset($_GET['search'])){
 					$search_query = $_GET['search'];
 
-					echo "<script>alert($search_query)</script>";
+					if(isset($_GET['sort'])){
+						$sort = $_GET['sort'];
+					}else{
+						$sort = "";
+					}
 
-					$get_b = "select * from books where book_title like '%$search_query%'";
+					//No filter.
+				$get_b = "select * from books where book_title like '%$search_query%'";
+
+
+				//<!--Author Filter  -->
+				if($sort == "author"){
+					$get_b = "SELECT * FROM books WHERE book_title like '%$search_query%' ORDER BY author";
+					//echo "<script>alert('Author Query')</script>";
+				}
+
+				//<!-- Low to High Price -->
+
+				if($sort == "low-price"){
+					$get_b = "SELECT * FROM books WHERE book_title like '%$search_query%' ORDER BY price ASC";
+				//	echo "<script>alert('low-price Query')</script>";
+				}
+				//<!-- High to Low Price -->
+
+				if($sort == "high-price"){
+					$get_b = "SELECT * FROM books WHERE book_title like '%$search_query%' ORDER BY price DESC";
+				//	echo "<script>alert('high-price Query')</script>";
+				}
+				//<!-- Year: new to old -->
+
+				if($sort == "new-release"){
+					$get_b = "SELECT * FROM books WHERE book_title like '%$search_query%' ORDER BY year DESC";
+				//	echo "<script>alert('new-release Query')</script>";
+				}
+				//<!-- Year: old to new -->
+				if($sort == "old-release"){
+					$get_b = "SELECT * FROM books WHERE book_title like '%$search_query%' ORDER BY year ASC";
+				//	echo "<script>alert('old-release Query')</script>";
+				}
+
+				//<!-- Book Title  -->
+				if($sort == "title"){
+					$get_b = "SELECT * FROM books WHERE book_title like '%$search_query%' ORDER BY book_title";
+				//	echo "<script>alert('Title Query')</script>";
+				}
+
+
+
+
 
 
 				$run_b = mysqli_query($con, $get_b);
-
 				while($row_b=mysqli_fetch_array($run_b)){
-
 						//initializing variable with book name.
 						$b_title = $row_b['book_title'];
 						$b_author = $row_b['author'];
 						$b_genre = $row_b['genre'];
-						$b_release = $row_b['release_date'];
+						$b_release = $row_b['year'];
 						$b_price = $row_b['price'];
 						$b_rating = $row_b['rating'];
 						$b_image = $row_b['book_image'];
-
 						//primary key
 						//used to display individual details page.
 						$b_isbn = $row_b['isbn'];
-
-
-
-
 						echo "
 							<div id='single_book'>
 
 								<h3>$b_title</h3>
 								<a href='details.php?b_isbn=$b_isbn'><img src='admin/book_images/$b_image' width='150px' height='200px'  /></a>
 								<p> $ $b_price </p>
+								<p>Author: $b_author</p>
+								<p>Year: $b_release</p>
+
 
 								<div style='margin: auto;'>
 								<a href='details.php?b_isbn=$b_isbn' style='float:left;'>More Info</a>
@@ -59,14 +101,9 @@
 
 								</div>
 							</div>
-
 						";
-
 					}
-
 				}
-
-
 
 				 ?>
 
