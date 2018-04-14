06584dec3a781a081c2d20e0f07a7f90c82cd0fa
Ronny Pacheco <ronny.pach@gmail.com>
2018-04-14 00:18:55 -0700

    Genres.
    Went back to old implementation of Genres, could not figure out the dropdown.

--- html/header.php
+++ html/header.php
@@ -38,40 +38,37 @@
         <a href="index.php">Home</a>
 				<a href="top_rated.php"> Top Rated </a>
 				<a href="top_sellers.php"> Top Sellers </a>
-				<a href="genres.php"> Genres </a>
-
-				<div class="dropdown">
-				  <button class="dropbtn">Genres</button>
-				  <div class="dropdown-content">
-
-						<?php
-
-						//getting and displying genres
-							$get_genres = "select * from book_genres";
-							$run_genres =  mysqli_query($con, $get_genres);
-							//loop that runs depending on the amount of rows on the book_genres table
-
-							while($row_genres=mysqli_fetch_array($run_genres)){
-								//fetching the information from the genre_type table on database
-								$genre_id = $row_genres['genre_id'];
-								$genre_type = $row_genres['genre_type'];
-								//display genres
-								echo "<a href=genres.php?genre=$genre_id>$genre_type </a>";
-
-							}
-						?>
-					 </div>
-				</div>
+				<a href="genres.php">Genres </a>
 
 				<!--
+        <a href="genres.php"> Genres </a>
 
 
+
+			/*
+				//getting and displying genres
+					$get_genres = "select * from book_genres";
+					$run_genres =  mysqli_query($con, $get_genres);
+					//loop that runs depending on the amount of rows on the book_genres table
+
+					while($row_genres=mysqli_fetch_array($run_genres)){
+						//fetching the information from the genre_type table on database
+						$genre_id = $row_genres['genre_id'];
+						$genre_type = $row_genres['genre_type'];
+						//display genres
+						echo "<a href=genres.php?genre=$genre_id>$genre_type </a>";
+					}
+
+
+				?>
         <form class="user" action="genres.php">
         	<label for="label_genre"><b><font face="helvetica">Genres</font></b></label>
         	<select name="genre">
-	        <?php
 	          $get_genres = "select * from book_genres";
 	          $run_genres =  mysqli_query($con, $get_genres);
+
+
+
 	          //loop that runs depending on the amount of rows on the book_genres table
 
 	          while($row_genres=mysqli_fetch_array($run_genres)){
@@ -79,16 +76,26 @@
 	            $genre_id = $row_genres['genre_id'];
 	            $genre_type = $row_genres['genre_type'];
 	            //display genres
-	            echo "<option value='$genre_id'> $genre_type</option>";
+	            echo "<option value='$genre_id'>$genre_type</option>";
 	          }
+
+
+
 					?>
           <input type="submit" value="Go" />
 					</select>
         <form>
--->
+
+				*/
+				?>
+
+			-->
+
 
 
 	      <a href="shoppingCart.php" style="float: right"> Shopping Cart</a>
+
+
       </div>
     </div>
   </body>
